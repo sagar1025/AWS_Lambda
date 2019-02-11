52761f56e67c6628721f8f2564c54583decd4929
sagar1025 <sagarpatel1025@gmail.com>
2019-02-10 22:58:21 -0500

    updated structure of request

--- app.py
+++ app.py
@@ -8,20 +8,31 @@ app = Chalice(app_name='sent')
 @app.route('/articleScore', methods=['POST'], cors=True)
 def index():
     request = app.current_request.json_body
-    score = 0.0
-    validArticle = {}
+    validArticle = []
     index = 0
-    for x in request['articles']:
+    for x in request:
         comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
         description = x['description']
-        response = comprehend.detect_sentiment(Text=description, LanguageCode='en')
+        if(description is not None and len(description) > 0):
+            responseDesc = comprehend.detect_sentiment(Text=description, LanguageCode='en')
 
-        #print(response['Sentiment'])
-        #print(response['SentimentScore']['Positive'])
-        if response['Sentiment'] == "NEUTRAL":
-            validArticle[index] = x
-        elif response['SentimentScore']['Neutral'] >= 0.48:
-            validArticle[index] = x
+        title = x['articleTitle']
+        if(title is not None and len(title) > 0):
+            responseTitle = comprehend.detect_sentiment(Text=title, LanguageCode='en')
+
+        content = x['content']
+        if(content is not None and len(content) > 0):
+            responseContent = comprehend.detect_sentiment(Text=content, LanguageCode='en')
+
+
+        if responseDesc['Sentiment'] == "NEUTRAL" or responseContent['Sentiment'] == "NEUTRAL":
+            validArticle.insert(index, x)
+        elif responseDesc['SentimentScore']['Neutral'] >= 0.47:
+            validArticle.insert(index, x)
+        elif responseTitle['SentimentScore']['Neutral'] >= 0.45:
+            validArticle.insert(index, x)
+        elif responseContent['SentimentScore']['Neutral'] >= 0.47:
+            validArticle.insert(index, x)
 
         index += 1
 
